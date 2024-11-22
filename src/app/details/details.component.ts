613155dc295190a872b47263600a40837fd0f5c3
Wilky <williewilkerson27@gmail.com>
2024-11-21 20:02:59 -0500

    I successfully set up the services and injections and now when a user clicks on a listing, it displays the listing and all information about it.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,25 +1,48 @@
 import { Component, inject } from '@angular/core'; // inject: Function to inject dependencies (new way of dependency injection)
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works! {{ housingLocationId }} <!-- {{ housingLocationId }} is interpolation syntex!! -->
-    </p>
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{housingLocation?.available_units}}</li>
+        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button">Apply Now</button>
+    </section>
+    </article>
   `,
   styleUrls: ['./details.component.css'] // Array of CSS files that style this component
 })
 export class DetailsComponent {
   // Inject the ActivatedRoute service using the new inject function
   // This gives access to the current route information
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId: number;
+  route: ActivatedRoute = inject(ActivatedRoute); // For accessing URL parameters
+  housingService = inject(HousingService);  // Service for housing data
+  housingLocation : HousingLocation | undefined; // Property to store housing details
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id'])
+    // Get the ID from the URL parameters and convert to number
+    const housingLocationId = Number(this.route.snapshot.params['id']); 
+    // Fetch housing location details using the ID
+    this.housingLocation = this.housingService.getAllHousingLocationById(housingLocationId)
   }
 }
